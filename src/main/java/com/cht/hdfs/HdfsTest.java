44bfb7eb43edc87157d3167451144dc6cffacdf7
chenghongtao <18729553226@163.com>
2020-03-20 14:52:32 +0800

    删除输出语句

--- src/main/java/com/cht/hdfs/HdfsTest.java
+++ src/main/java/com/cht/hdfs/HdfsTest.java
@@ -10,11 +10,8 @@ import java.net.URISyntaxException;
 
 public class HdfsTest {
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, URISyntaxException {
-//        Configuration conf = new Configuration();
-//        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.2.10:9000"), conf, "root");
-//        fs.copyFromLocalFile(new Path("D:\\java\\src.zip"), new Path("/"));
-        System.out.println(System.getProperty("HADOOP_HOME_USER"));
-        System.out.println(System.getProperty("user.name"));
-
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.2.10:9000"), conf, "root");
+        fs.copyFromLocalFile(new Path("D:\\java\\src.zip"), new Path("/"));
     }
 }
