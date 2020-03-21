7874dcfe0ca626f8d04c5ac1690f0fc5fa755436
chenghongtao <18729553226@163.com>
2020-03-21 15:31:22 +0800

    更新

--- src/main/java/com/cht/hdfs/HdfsTest.java
+++ src/main/java/com/cht/hdfs/HdfsTest.java
@@ -9,7 +9,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class HdfsTest {
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException, URISyntaxException {
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(new URI("hdfs://192.168.2.10:9000"), conf, "root");
         fs.copyFromLocalFile(new Path("D:\\java\\src.zip"), new Path("/"));
