e30d5ae7a73b38093a811eb132e51140a485bd00
chenghongtao <18729553226@163.com>
2020-03-22 18:01:40 +0800

    修改问题

--- src/main/java/com/cht/hdfs/HdfsTest.java
+++ src/main/java/com/cht/hdfs/HdfsTest.java
@@ -11,7 +11,7 @@ import java.net.URISyntaxException;
 public class HdfsTest {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
         Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.2.10:9000"), conf, "root");
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.2.10:9000"), conf,"root");
         fs.copyFromLocalFile(new Path("D:\\java\\javafx-src.zip"), new Path("/"));
     }
 }
