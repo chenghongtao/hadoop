8be43868abbbb6ea79395c6e07e92ff0c1e619de
chenghongtao <18729553226@163.com>
2020-03-22 17:04:14 +0800

    修改获取fs报错DFS[null]的问题,版本冲突

--- src/main/java/com/cht/hdfs/HdfsTest.java
+++ src/main/java/com/cht/hdfs/HdfsTest.java
@@ -12,6 +12,6 @@ public class HdfsTest {
     public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(new URI("hdfs://192.168.2.10:9000"), conf, "root");
-        fs.copyFromLocalFile(new Path("D:\\java\\src.zip"), new Path("/"));
+        fs.copyFromLocalFile(new Path("D:\\java\\javafx-src.zip"), new Path("/"));
     }
 }
