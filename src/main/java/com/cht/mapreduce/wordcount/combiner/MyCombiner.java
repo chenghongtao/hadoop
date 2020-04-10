d28649f661270c5e299a472e793067e3320a83ce
chenghongtao <18729553226@163.com>
2020-04-10 15:39:18 +0800

    添加Combiner的相关注解说明

--- src/main/java/com/cht/mapreduce/wordcount/combiner/MyCombiner.java
+++ src/main/java/com/cht/mapreduce/wordcount/combiner/MyCombiner.java
@@ -6,7 +6,10 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 /*
-
+   Combiner是在每个节点先做的一个小范围的reduce，目的是减少shuffle阶段和reduce阶段的数据量，减小reduce阶段的压力。
+   1.实现Reducer类
+   2.在job中使用job.setCombinerClass(WordCountReduce.class)方法指定,
+   注意：一般combiner和reduce的逻辑一样，所以直接指定自定义的reduce类即可。
  */
 
 public class MyCombiner extends Reducer<Text, IntWritable,Text, IntWritable> {
